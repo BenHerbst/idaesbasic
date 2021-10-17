212ceef854663e02dec78c9422b263b3210509c0
BenHerbst <ben.herbst@web.de>
2021-10-17 12:34:46 +0200

    Added layout

--- lib/src/main/java/ainm/Main.java
+++ lib/src/main/java/ainm/Main.java
@@ -1,19 +1,28 @@
 package ainm;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage stage) {
-        Scene scene = new Scene(new StackPane(), 640, 480);
-        stage.setScene(scene);
-        stage.setTitle("Ainm - 0.01 / Todolist");
-        stage.setMaximized(true);
-        stage.show();
+    	try {
+    		Parent root = FXMLLoader.load(getClass().getResource("/MainView.fxml"));
+        	Scene scene = new Scene(root, 640, 480);
+        	stage.setScene(scene);
+        	stage.setTitle("Ainm - 0.01 / Todolist");
+        	stage.setMaximized(true);
+        	stage.show();
+    	} catch(Exception e) {
+    		e.printStackTrace();
+    	}
     }
 
     public static void main(String[] args) {
