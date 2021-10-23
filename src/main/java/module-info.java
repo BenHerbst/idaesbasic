202ac9856201c1310414ce6d53ade74554a983b6
BenHerbst <ben.herbst@web.de>
2021-10-23 22:10:15 +0200

    Added predefined calendars

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,6 +7,7 @@ module idaesbasic {
     requires org.kordamp.ikonli.core;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.ikonli.fontawesome;
+	requires com.calendarfx.view;
 
 	opens org.idaesbasic to javafx.fxml;
 	opens org.idaesbasic.controllers to javafx.fxml;
--- src/main/java/org/idaesbasic/controllers/MainController.java
+++ src/main/java/org/idaesbasic/controllers/MainController.java
@@ -41,6 +41,11 @@ import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import com.calendarfx.model.Calendar;
+import com.calendarfx.model.Calendar.Style;
+import com.calendarfx.model.CalendarSource;
+import com.calendarfx.view.CalendarView;
+
 public class MainController {
 
     List<String> registered_projects = new ArrayList<>();
@@ -273,6 +278,39 @@ public class MainController {
         Tab newTab = new Tab();
         newTab.setText("New tab");
         tabPane.getTabs().add(newTab);
+        CalendarView calendarView = new CalendarView();
+        //Create work calendars
+        Calendar meetingsCalendar = new Calendar("Meetings");
+        Calendar pausesCalendar = new Calendar("Pauses");
+        Calendar vacationsCalendar = new Calendar("Vacation");
+        meetingsCalendar.setStyle(Style.STYLE2);
+        pausesCalendar.setStyle(Style.STYLE1);
+        vacationsCalendar.setStyle(Style.STYLE3);
+        CalendarSource workCalendarSource = new CalendarSource("Work");
+        workCalendarSource.getCalendars().addAll(meetingsCalendar, pausesCalendar, vacationsCalendar);
+        //Create private calendars
+        Calendar birthdaysCalendar = new Calendar("Birthdays");
+        Calendar appointmentsCalendar = new Calendar("Appointments");
+        Calendar eventsCalendar = new Calendar("Events");
+        birthdaysCalendar.setStyle(Style.STYLE5);
+        appointmentsCalendar.setStyle(Style.STYLE6);
+        eventsCalendar.setStyle(Style.STYLE4);
+        CalendarSource privateCalendarSource = new CalendarSource("Private");
+        privateCalendarSource.getCalendars().addAll(birthdaysCalendar, appointmentsCalendar, eventsCalendar);
+        //Create sport calendars
+        Calendar cyclingCalendar = new Calendar("Cylcling");
+        Calendar joggingCalendar = new Calendar("Jogging");
+        Calendar swimingCalendar = new Calendar("Swiming");
+        cyclingCalendar.setStyle(Style.STYLE4);
+        joggingCalendar.setStyle(Style.STYLE7);
+        swimingCalendar.setStyle(Style.STYLE2);
+        CalendarSource sportCalendarSource = new CalendarSource("Sport");
+        sportCalendarSource.getCalendars().addAll(cyclingCalendar, joggingCalendar, swimingCalendar);
+        //Add calendars to view
+        calendarView.getCalendarSources().addAll(workCalendarSource, privateCalendarSource, sportCalendarSource);
+        //Update current calendar time
+        calendarView.setRequestedTime(LocalTime.now());
+        newTab.setContent(calendarView);
         // Select the tab
         SingleSelectionModel<Tab> selectionModel = tabPane.getSelectionModel();
         selectionModel.select(newTab);
