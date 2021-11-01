3e7b70c86a4c1c172f6b1f971bff1d911f77ede6
BenHerbst <ben.herbst@web.de>
2021-11-01 02:28:32 +0100

    Switch from eclipse to netbeans

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,25 +1,25 @@
 module idaesbasic {
-	requires java.desktop;
-	requires javafx.controls;
-	requires javafx.fxml;
-	requires javafx.base;
-	requires javafx.graphics;
+    requires java.desktop;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.base;
+    requires javafx.graphics;
     requires org.kordamp.ikonli.core;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.ikonli.fontawesome;
-	requires com.calendarfx.view;
-	requires org.json;
-    requires org.mnode.ical4j.core;
+    requires com.calendarfx.view;
+    requires org.json;
+    requires java.logging;
 
-	opens org.idaesbasic to javafx.fxml;
-	opens org.idaesbasic.controllers to javafx.fxml;
-	opens org.idaesbasic.controllers.todolist to javafx.fxml;
-	opens org.idaesbasic.models to org.json;
-	opens org.idaesbasic.controllers.calendar to javafx.fxml;
-	
-	exports org.idaesbasic;
-	exports org.idaesbasic.controllers;
-	exports org.idaesbasic.controllers.todolist;
-	exports org.idaesbasic.controllers.calendar;
-	exports org.idaesbasic.models;
+    opens org.idaesbasic to javafx.fxml;
+    opens org.idaesbasic.controllers to javafx.fxml;
+    opens org.idaesbasic.controllers.todolist to javafx.fxml;
+    opens org.idaesbasic.models to org.json;
+    opens org.idaesbasic.controllers.calendar to javafx.fxml;
+
+    exports org.idaesbasic;
+    exports org.idaesbasic.controllers;
+    exports org.idaesbasic.controllers.todolist;
+    exports org.idaesbasic.controllers.calendar;
+    exports org.idaesbasic.models;
 }
