72caf36af43634e491e8970b9caba87e0107c6cc
BenHerbst <ben.herbst@web.de>
2022-01-18 23:25:49 +0100

    Can load files now

--- src/main/kotlin/org/idaesbasic/buffer/NewBufferView.kt
+++ src/main/kotlin/org/idaesbasic/buffer/NewBufferView.kt
@@ -1,10 +1,15 @@
 package org.idaesbasic.buffer
 
+import javafx.beans.property.SimpleStringProperty
 import org.idaesbasic.MainView
 import org.idaesbasic.buffer.file.FileModel
 import tornadofx.*
+import java.nio.file.Files
+import java.nio.file.Paths
 
 class NewBufferView : Fragment () {
+    val loadDirectory = SimpleStringProperty()
+
     override val root = squeezebox {
         fold("New file", expanded = true) {
             form {
@@ -34,7 +39,7 @@ class NewBufferView : Fragment () {
                 fieldset ("Location") {
                     field ("Directory") {
                         hbox {
-                            textfield { }
+                            textfield (loadDirectory)
                             button {  }
                         }
                     }
@@ -42,7 +47,8 @@ class NewBufferView : Fragment () {
                 button("Load file") {
                     action {
                         val mainView = find(MainView::class)
-                        val file = FileModel(null, null, null)
+                        val openedFile = Paths.get(loadDirectory.value)
+                        val file = FileModel(openedFile.fileName.toString(), openedFile, Files.readString(openedFile))
                         mainView.newEditor(mainView.controller.currentBufferIndex, file)
                         mainView.controller.openCurrentBufferIndexBuffer()
                     }
